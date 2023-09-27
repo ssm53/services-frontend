7c68248ddb365f8b20a4a0f75153f94cdb4b3641
root <root@DESKTOP-J195E5S>
2023-09-27 15:56:01 +0800

    Franc done User & Seller login

---
+++ src/stores/store.js
@@ -0,0 +1,7 @@
+export const userLoggedIn = writable(false);
+
+export const sellerLoggedIn = writable(false);
+
+export const loading = writable(false);
+
+export const filter = writable(false);
---
+++ src/utils/auth.js
@@ -0,0 +1,131 @@
+import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
+// store added for log in
+import { sellerLoggedIn } from '../stores/store';
+import { userLoggedIn } from '../stores/store';
+
+// export function getSellerId() {
+//   const auth = localStorage.getItem("auth")
+//   if (auth) {
+//     return JSON.parse(auth)["sellerId"]
+//   }
+//   return null
+// }
+
+// export function getTokenFromLocalStorage() {
+//   const auth = localStorage.getItem("auth")
+//   if (auth) {
+//     return JSON.parse(auth)["token"]
+//   }
+//   return null
+// }
+
+// export async function isLoggedIn() {
+//   if (!getTokenFromLocalStorage()) {
+//     return false
+//   }
+
+//   try {
+//     const resp = await fetch(
+//       PUBLIC_BACKEND_BASE_URL + '/api/collections/users/auth-refresh',
+//       {
+//         method: 'POST',
+//         mode: 'cors',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Authorization': getTokenFromLocalStorage()
+//         },
+//       }
+//     );
+
+//     const res = await resp.json()
+//     if (resp.status == 200) {
+
+//       localStorage.setItem("auth", JSON.stringify({
+//         "token": res.token,
+//         "sellerId": res.record.id
+//       }));
+
+//       return true
+//     }
+
+//     return false
+//   } catch {
+//     return false
+//   }
+// }
+// checks if username and password matches api database then log in user
+export async function authenticateSeller(email, password) {
+  const resp = await fetch(
+    PUBLIC_BACKEND_BASE_URL + '/auth-seller',
+    {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    }
+  );
+
+  const res = await resp.json();
+
+  if (resp.status == 200) {
+    localStorage.setItem("auth", JSON.stringify({
+      sellerToken: res.sellerAccessToken,
+      sellerId: res.sellerId
+    }));
+
+sellerLoggedIn.set(true)
+
+    return {
+      success: true,
+      res: res
+    }
+  }
+
+  return {
+    success: false,
+    res: res
+  }
+}
+
+export async function authenticateUser(email, password) {
+  const resp = await fetch(
+    PUBLIC_BACKEND_BASE_URL + '/auth-user',
+    {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    }
+  );
+
+  const res = await resp.json();
+
+  if (resp.status == 200) {
+    localStorage.setItem("auth", JSON.stringify({
+      userToken: res.userAccessToken,
+      userId: res.userId
+    }));
+
+userLoggedIn.set(true)
+
+    return {
+      success: true,
+      res: res
+    }
+  }
+
+  return {
+    success: false,
+    res: res
+  }
+}
