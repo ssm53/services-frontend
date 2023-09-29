c4f1c185df850cdef79d8456ae1d461271c8e8e3
root <root@DESKTOP-J195E5S>
2023-09-29 17:20:42 +0800

    Frankie user click seller profile

--- src/routes/user/[slug]/+page.js
+++ src/routes/user/[slug]/+page.js
@@ -1,19 +1,20 @@
 import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
 
 export async function load({ fetch, params }) {
-  const resp = await fetch(PUBLIC_BACKEND_BASE_URL + `/api/collections/jobs/records/${params.slug}`);
+
+  const resp = await fetch (PUBLIC_BACKEND_BASE_URL + `/getSeller/${params.slug}`)
 
   const res = await resp.json();
+  
   if (resp.status == 200) {
     return {
-      job: res
+      seller: res.thatSeller
     }
   } else {
     return {
-      jobs: []
+      seller: []
     }
   }
 }
 
 
-// "jobs" may need to change to something else
