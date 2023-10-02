2793b3fef71dcd5bb74461d6a38d4f4937880fdd
hemoiswhat <heng.j.yap@gmail.com>
2023-10-02 17:39:10 +0800

    update_styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,28 @@ import { goto } from '$app/navigation';
 function goToSeller(){
   goto ("/seller")
 }
+
+function goToUser(){
+  goto ("/user/[slug]")
+}
 </script>
 
 <main>
+  <div class ="bg-green-400">
+    <h2 class= "italic text-5xl bg-green-400 mx-auto my-auto flex justify-center"> X Hack </h2>
+        <p class="  flex items-center justify-center">
+            A platform which bring you the best coding teacher
+        </p>
+</div>
       <button on:click={goToSeller}
       class = "bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md"
       >Go to Seller
 </button>
+<button on:click={goToUser}
+class = "bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md"
+>Go to User
+</button>
+
    <div>this is the main page for seller and users</div> 
   </main>
 
--- src/utils/HeaderSeller.svelte
+++ src/utils/HeaderSeller.svelte
@@ -24,57 +24,23 @@
   
 
 </script>
-<nav>
-    <div class="container">
-    <h1>X Hack</h1>
-    
-        <button class= "bg-[#a6c1ee] text-white px-5 py-2  rounded-full hover:bg-[#87acec] " on:click={sellerHome}>Home</button>
+<nav class="bg-blue-700">
+    <div class="flex justify-between items-center w-[92%]  mx-auto">
+    <h1 class="px-100 italic font-black font-2xl ">X Hack</h1>
+    <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+        <button class= "bg-[#a6c1ee] text-black px-5 py-2  rounded-full hover:bg-[#87acec] " on:click={sellerHome}>Home</button>
         
         {#if logIO === true}
       <div class = "self-auto">  <button on:click={sellerLogOut}>Log out</button> </div>
         {:else}
-        <button class="bg-[#a6c1ee] text-white px-5 py-2  rounded-full hover:bg-[#87acec] " on:click={clickLoggedIn}>Log in</button>
-        <button class="bg-[#a6c1ee] text-white px-5 py-2  rounded-full hover:bg-[#87acec] " on:click = {sellerSignup}>Sign Up</button>
-    
-{/if}
+        <button class="bg-[#a6c1ee] text-black px-5 py-2  rounded-full" on:click={clickLoggedIn}>Log in</button>
+        <button class="bg-[#a6c1ee] text-black px-5 py-2  rounded-full hover:bg-[#87acec] " on:click = {sellerSignup}>Sign Up</button>
+        {/if}
+      </ul>
 </div>
 </nav>
 
 <style>
-    nav {
-        background-color: rgb(47, 1, 119);
-        color : white;
-    }
-    .container{
-        display: flex;
-        align-items: flex;
-    }
-    ul {
-    display:  flex;
-    margin: 10px;
-    list-style :none;
-    margin-left:auto ;
-    font-size:1em;
-    }   
-    li {
-        margin-right :0 ;
-        font-size : 1.0em;
-        font-weight: normal;
-    }  
-    h1 {
-        margin: 10px;
-        font-size : 1.0em;
-        font-weight: normal;
-        margin-left : 150px;
-    }
-    a{
-        text-decoration: none;
-        color: #aaa(245, 241, 241);
-        margin-left: 50px;
-    }
-    .active{
-        color : white(199, 15, 15);
-        margin-left : 10px
-    }
+
 
   </style>
